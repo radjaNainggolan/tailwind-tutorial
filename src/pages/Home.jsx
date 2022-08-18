be4b80d8274fe39f0a2385224bb85e8b584bf43b
Raden <radenrovcanin12@gmail.com>
2022-08-18 15:12:20 +0200

    icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,16 +15,19 @@ const Home = () => {
                         <ul>
                             <li className="text-gray-700 font-bold">
                                 <Link to='#'>
+                                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
                                     <span>Home</span>
                                 </Link>
                             </li>
                             <li>
                                 <Link to='#'>
+                                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                                     <span>About</span>
                                 </Link>
                             </li>
                             <li>
                                 <Link to='#'>
+                                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg>
                                     <span>Contacts</span>
                                 </Link>
                             </li>
@@ -61,6 +64,7 @@ const Home = () => {
                                     <span className="block text-gray-500 text-sm">Recepie by Mario</span>
                                 </div>
                                 <div className="badge">
+                                    <svg className="w-6 h-6 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                                     <span>25 min</span>
                                 </div>
                             </div>
@@ -71,6 +75,7 @@ const Home = () => {
                                     <span className="block text-gray-500 text-sm">Recepie by Mario</span>
                                 </div>
                                 <div className="badge">
+                                    <svg className="w-6 h-6 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                                     <span>25 min</span>
                                 </div>
                             </div>
@@ -81,6 +86,7 @@ const Home = () => {
                                     <span className="block text-gray-500 text-sm">Recepie by Mario</span>
                                 </div>
                                 <div className="badge">
+                                    <svg className="w-6 h-6 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                                     <span>25 min</span>
                                 </div>
                             </div>
