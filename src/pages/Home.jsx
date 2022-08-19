014e33e00e4600b9036a61e20d0fa5bf4892ffeb
Raden <radenrovcanin12@gmail.com>
2022-08-19 15:10:44 +0200

    responsive nav part1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,32 +3,35 @@ const Home = () => {
     return (
         <div className="text-gray-700 bg-gray-200 font-body">
             <div className="grid md:grid-cols-3">
-                <div className="md:col-span-1">
-                    <nav>
+                <div className="md:col-span-1 md:flex md:justify-end">
+                    <nav className="text-right">
                         <div>
                             <h1 className="font-bold uppercase p-4 border-b border-gray-100">
                                 <Link to='/' className="text-green-500 sm:text-red-700 lg:text-blue-700 text-sm md:text-xl hover:text-gray-700">
                                     Food ninja
                                 </Link>
                             </h1>
+                            <div  className="md:hidden">
+                                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
+                            </div>
                         </div>
-                        <ul>
+                        <ul className="hidden md:block md:text-sm md:mt-6 md:space-y-3">
                             <li className="text-gray-700 font-bold">
-                                <Link to='#'>
-                                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+                                <Link to='#' className=" px-4 flex justify-end active:border-r-2 active:border-amber-600" >
                                     <span>Home</span>
+                                    <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
                                 </Link>
                             </li>
                             <li>
-                                <Link to='#'>
-                                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                                <Link to='#' className="px-4 flex justify-end">
                                     <span>About</span>
+                                    <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                                 </Link>
                             </li>
                             <li>
-                                <Link to='#'>
-                                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg>
+                                <Link to='#' className="px-4 flex justify-end">
                                     <span>Contacts</span>
+                                    <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg>
                                 </Link>
                             </li>
                         </ul>
@@ -37,8 +40,8 @@ const Home = () => {
 
                 <main className='px-16 py-6 md:col-span-2'>
                     <div className="flex text-orange-200 justify-center md:justify-end">
-                        <Link to='#' className="btn md:border-amber-300 md:border-2 hover:bg-amber-200 hover:text-white">Log in</Link>
-                        <Link to='#' className="btn ml-2 md:border-amber-300 md:border-2 hover:bg-amber-200 hover:text-white ">Sign up</Link>
+                        <Link to='#' className="btn md:border-amber-300 md:border-2 hover:bg-amber-200 hover:text-white transition ease-out delay-100">Log in</Link>
+                        <Link to='#' className="btn ml-2 md:border-amber-300 md:border-2 hover:bg-amber-200 hover:text-white transition ease-out delay-100">Sign up</Link>
                     </div>
 
                     {/* <div className="flex items-end">
